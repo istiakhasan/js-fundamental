eaa0ea32e7d4fe995f96d149b692a49f05938671
Istieak <85983888+Istieak@users.noreply.github.com>
2022-02-01 13:55:50 +0600

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+/* 
+//0,1,1,2,3,5,8,13,21,34,55
+3rd=2nd+1st
+4th=3rd+2nd
+5th=4th+3rd
+4th=3rd+2nd
+16=15th+14th
+119th=118th+117th
+nth=(n-1)th+(n-2)th
+ith=(i-1)th+(i-2)th
+fibonacci series er jonno 3rd index  thekey suru kortey hbe
+*/
+
+const fibo=[0,1]
+for (let i=2;i<=10;i++){
+    fibo[i]=fibo[i-1]+fibo[i-2]
+}
+console.log(fibo)
